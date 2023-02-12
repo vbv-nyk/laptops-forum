0a740e45a8ab6230916d2130a47186ffef3c88f4
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2023-02-12 23:34:31 +0530

    Add user update

--- controllers/User.js
+++ controllers/User.js
@@ -4,11 +4,22 @@ const User = require("../models/User");
 
 const userRouter = express.Router();
 
+userRouter.get("/:username", async (req, res) => {
+    const { username } = req.params.username;
+    const user = await User.find({ username });
+    res.send(user);
+})
+
+userRouter.get("/", async (req, res) => {
+    const user = await User.find({});
+    res.send(user);
+})
+
 userRouter.post("/signup", async (req, res) => {
     const { firstName, username, lastName, password, laptopsOwned } = req.body;
     const checkUsername = await User.find({ username });
     if (checkUsername.length) {
-        res.send({
+        return res.send({
             error: "Username already exists",
         })
     }
@@ -18,5 +29,17 @@ userRouter.post("/signup", async (req, res) => {
     user = await User.findById(userId).populate({ path: "laptopsOwned", select: ["name", "link"] })
     res.send(user);
 });
+
+userRouter.put("/update/:username", async (req, res) => {
+    const { username } = req.params;
+    const { firstName, lastName, password, laptopsOwned } = req.body;
+    const checkUsername = await User.findOne({ "username": username });
+    console.log(checkUsername);
+    if (!checkUsername.username)
+        return res.send({ error: "User not found" });
+    const hashedPassword = await bcrypt.hash(password, 8);
+    const user = await User.findByIdAndUpdate(checkUsername._id, { firstName, username, lastName, "password": hashedPassword, laptopsOwned })
+    res.send(user);
+})
 
 module.exports = userRouter;
